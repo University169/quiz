f8b99e6de469b49f69b6470da9572c31e5f8f824
dmitriy.klimkov <dmitriy.klimkov@rt.ru>
2020-07-11 21:54:29 +0300

    Добавил polls в urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+# указание корневого URLconf на модуль polls.urls
+urlpatterns += [
+    path('polls/', include('polls.urls')),
 ]
